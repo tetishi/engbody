5902ddfdcefb8f5949cac748bc350ea403d275bc
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2023-07-31 12:31:12 +0900

    add animation for fv

---
+++ .history/js/script_20230730140738.js
@@ -0,0 +1,17 @@
+//header
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 250) {
+            $('.counseling-btn-top').css('opacity', 1);
+            $('.header').css('background-color', '#fff');
+        } else {
+            $('.counseling-btn-top').css('opacity', 0);
+            $('.header').css('background-color', 'transparent');
+        }
+    })
+})
+
+//背景色が伸びて出現
+function BgFadeAnime() {
+    
+}
